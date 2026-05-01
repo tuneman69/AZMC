165fd67c8877e5da0e6a447a65a27508846dbbdd
tuneman69 <bitnert@gmail.com>
2026-04-30 18:40:31 -0700

    Update __manifest__.py

--- compliance_training/__manifest__.py
+++ compliance_training/__manifest__.py
@@ -3,7 +3,7 @@
     'name': "Compliance Training",
     'version': '19.0.1.0.0',
     'summary': "Competency & Training Management System",
-    'author': "T&M Consulting",h
+    'author': "T&M Consulting"
     'category': 'Human Resources',
     'license': 'OPL-1',
     'price': 149.00,
